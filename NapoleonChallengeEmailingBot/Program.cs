faa4b78e470343cad37be70d21bace44d9fec633
Arfaoui Med Amine <mohamedamine.arfaoui@agilz.net>
2021-09-22 04:16:41 +0100

    Initial commit

---
+++ NapoleonChallengeEmailingBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace NapoleonChallengeEmailingBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mandrillEmail email = new mandrillEmail();
+            HtmlWeb binancewebsite= new HtmlWeb();
+            HtmlDocument document =  binancewebsite.Load("https://www.binance.com/en/futures-activity/leaderboard?type=myProfile&encryptedUid=8D27A8FA0C0A726CF01A7D11E0095577");
+
+           
+            var message = "hold on Napoleon there an update and you should get notified by mail";
+            var listInitial = document.DocumentNode.SelectNodes("//div[@data-bn-type='text']").ToList();
+            var c= 10;
+            for (int i = 0; i < c; i++)
+            {
+                HtmlDocument documentUpdated =  binancewebsite.Load("https://www.binance.com/en/futures-activity/leaderboard?type=myProfile&encryptedUid=8D27A8FA0C0A726CF01A7D11E0095577");
+                var listUpdated = document.DocumentNode.SelectNodes("//div[@data-bn-type='text']").ToList();
+                if (listInitial!=listUpdated)
+                {
+                   Console.WriteLine(message);
+                   email.SendUpdatingMail(message,"Napoleon","mohamedamine.arfaoui@esprit.tn");
+                }
+                c++;
+
+            }
+        }
+    }
+}
---
+++ NapoleonChallengeEmailingBot/mandrillEmail.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using Mandrill;
+using Mandrill.Models;
+using Mandrill.Requests.Messages;
+
+public class mandrillEmail
+{
+    public async void SendUpdatingMail(string message, string from,string to)
+    {
+        MandrillApi api = new MandrillApi("1651d8f826afa14ff4d978a7e63c53fd-us5");
+        UserInfo info = await api.UserInfo();
+        Console.WriteLine(info.Username);
+
+        var email = new EmailMessage();
+
+        var correos = new List<EmailAddress>();
+        correos.Add(new EmailAddress(to));
+
+        email.To = correos;
+
+        email.Subject = String.Format("there was an update");
+
+        email.FromEmail = from;
+        email.FromName = "Napoleon";
+
+
+        email.TrackOpens = true;
+        email.Html =
+            $"hello ";
+
+        await api.SendMessage(new SendMessageRequest(email));
+    }
+}
